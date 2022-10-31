9a6e6f4d274e598ee7c40e883507ef854abe8dca
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-10-31 14:03:05 -0400

    added portfolio for projects

--- app.js
+++ app.js
@@ -2,14 +2,100 @@
 
 import inquirer from 'inquirer';
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some info:'
+
+        }
+    ]);
+};
+// promptUser().then(answers => console.log(answers));
+
+///Add project information
+const promptProject = portfolioData => {
+    //if there is no projects arrray property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-])
-    .then(answers => console.log(answers));
+    console.log(
+        `
+        ==================
+        Add a New Project
+        ==================
+        `
+    );
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project'
+
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build the project with?Check all that applies.',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'insert the github link for your project'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature the project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'would you like to enter another project?',
+            default: false
+        }
+    ])
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
+};
+// promptProject().then(ProjectAnswers => console.log(projectAnswers));
+promptUser()
+    // .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
+    // .then(projectAnswers => console.log(projectAnswers));
+
+
+    //======================================================================================
 // const promptUser = () => {
 
 //     return inquirer.prompt([
